06eabe036ed73f8665fbe794d03990d12b4e3f42
venusyprime <venusyprime@users.noreply.github.com>
2015-11-15 09:15:29 +0000

    Changed ConvertFrom-SteamFile for better support
    ConvertFrom-SteamFile now converts the files into an XML format rather
    than just trying to add the properties. This allows it to work properly
    with Steam Controller files.

--- Steam.psm1
+++ Steam.psm1
@@ -62,16 +62,26 @@ function Get-SteamGame {
                     $select = $dir | select-string $Name
                     $pathtemp = $select.Path | select -Unique
                     $dir = foreach ($file in $pathtemp) {$dir | where FullName -eq $file}
+                    if ($dir -eq $null) {Write-Error "No games found in local Steam libraries that match Name $name."}
                 }
-                if ($AppID) {Write-Verbose "AppID is $AppID";$dir = $dir | where Name -like "*_$($AppID).acf"}
+                if ($AppID) {
+                    Write-Verbose "AppID is $AppID"
+                    $dir = $dir | where Name -like "*_$($AppID).acf"
+                    if ($dir -eq $null) {Write-Error "No games found in local Steam libraries that match AppID $AppID."}
+                }
                 $dir
             } @paramsandpath
         }
 
         $convertedall = ConvertFrom-SteamFile -Path $manifests.FullName @params
         $objects = foreach ($converted in $convertedall) {
+            $converted = $converted.AppState
+            
+            [string]$Name = $converted.Name
+            if ($Name -eq "AppState") {$name = $converted.UserConfig.Name}
             $manifest = $manifests | where Name -Like "*_$($converted.AppID).acf"         
-            $AppInstallDir = $converted.AppInstallDir
+            
+            $AppInstallDir = $converted.UserConfig.AppInstallDir
             if ($AppInstallDir -eq $null) {
                 $InstallDirBase = $converted.InstallDir
                 if ($installdirbase -like "*\*") {$InstallDir = $InstallDirBase.Replace("\\","\")}
@@ -85,7 +95,7 @@ function Get-SteamGame {
             [double]$SizeOnDiskMB = "{0:N2}" -f ($SizeOnDiskBytes/1MB)
 
             $object = [PSCustomObject]@{
-                Name = $Converted.Name
+                Name = $Name
                 AppID = $Converted.AppID
                 LastUpdated = $LastUpdatedDateTime
                 SizeOnDisk = $SizeOnDiskMB
@@ -130,6 +140,75 @@ function ConvertFrom-SteamFile {
             Write-Verbose "Looking at $item on $env:computername."
             $content = Get-Content $item
             if ($content -ne "") {
+                $idarray = [ordered]@{}
+                $var1 = 0
+                $content2 = foreach ($line in $content) {
+                    if (($line.Contains('"')) -eq $true) {
+                        $split = $line.Split('"')
+                        $name = $line.Split('"')[1]
+                        #bugfix: cannot have a number as an ID name
+                        foreach ($number in 0..9) {
+                            if ($name -like "$number*") {$name = "ID_$name"}
+                        }
+                        switch ($split.Count) {
+                            3 {
+                                #if test is here for any in the format "name" 1
+                                if ($split[2] -eq "") {
+                                    "<$name>"
+                                    #Write-Warning "Writing $name to IDArray"
+                                    $idarray = $idarray+@{$var1 = "</$name>"}
+                                    $var1++
+                                }
+                                else {$value = $split[2].Trim(" ");"<$name>"+$value+"</$name>"}
+                            }
+                            5 {
+                                $value = $split[3]
+                                "<$name>"+$value+"</$name>"
+                            }
+                        }
+                    }
+                    else {$line}
+                }
+                $idarraytemp = $idarray.GetEnumerator()
+                $matches_open = $content2 | select-string "{" | select Line,LineNumber
+                $matches_closed = $content2 | select-string "}" | select Line,LineNumber
+                $content3 = $content2
+
+                foreach ($match in $matches_open) {
+                        $linenumber = $match.LineNumber - 1
+                        $line = $match.Line.Replace("{","}")
+    
+                        $closedline = $matches_closed | where Line -eq $Line | select -First 1
+                        $closedlinenumber = $closedline.LineNumber - 1
+                        $id = $idarraytemp | select -First 1
+
+                        $content3[$linenumber] = ""
+                        $content3[$closedlinenumber] = $id.Value
+                        $matches_closed = $matches_closed | where LineNumber -ne $closedLine.LineNumber
+                        $idarraytemp = $idarraytemp | where Name -ne $id.Name
+                        Write-Verbose "Open:  $linenumber, ID: $($content3[$linenumber-1])"
+                        Write-Verbose "Close: $closedlinenumber, ID $($id.value)"
+                }
+                #bugfix: for steam files that have no root node
+                Write-Debug "Before content3 becomes a string"
+                $content3 = @"
+<xml>
+$($content3 | Out-String)
+</xml>
+"@
+                Write-Debug "before ampersand replacement"
+                $content3 = $content3.Replace("&","&amp;")
+                $xml = [xml]$content3
+                $XML.Xml
+            }
+            if ($content -eq "") {Write-Warning "$item is null or empty."}
+
+
+        <#
+        foreach ($item in $path) {
+            Write-Verbose "Looking at $item on $env:computername."
+            $content = Get-Content $item
+            if ($content -ne "") {
                 $object = [PSCustomObject]@{}
                 foreach ($entry in $content) {
                     if ($entry -like '*"*') {
@@ -143,6 +222,8 @@ function ConvertFrom-SteamFile {
                 $object #| Select-Object -Property * -Unique
             }
             if ($content -eq "") {Write-Warning "$item is null or empty."}
-        }  
+        }
+        #>  
+        }
     } 
 }
